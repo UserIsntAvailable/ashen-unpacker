502ed5a6376c0c1527a5fe3377aa4e855c0f0a72
UserIsntAvailable <pinedap.angel@gmail.com>
2023-03-08 01:23:45 -0500

    refactor(pman)!: copyright max size is now `55` bytes.
    Decided to change the semantics of the file format spec, since forcing the user
    to use a NULL terminated string is way much easier than trying to guess were the
    copyright boundary ends; it also doesn't really affect the original pman file,
    but I want to have a clean parser implementation? I will need to make a PR to
    upstream to inform on the README that that is the "unofficial", but "technically
    right" intention.
    
    Also, I added some extra documentation to make explicit that String::len != # of
    chars, to avoid future confusions.
    
    refactor: use byte string literals instead of byte arrays.
    
    refactor: rename `parse_*` functions to `read_*`.
    
    Might change this back to `parse_*`, but for now I think this is a better
    naming.
    
    tests: include the full function name of the fuction being tested.
    
    Before I was dropping the `parse_*`, which made reading test names kinda weird.

--- src/format/pman.rs
+++ src/format/pman.rs
@@ -1,15 +1,20 @@
 use super::Result;
 use flate2::read::ZlibDecoder;
 use nom::{
-    bytes::complete::{tag, take, take_till},
+    bytes::complete::{tag, take},
+    character::complete::char,
+    combinator::eof,
     multi::fill,
     number::complete::le_u32,
+    sequence::terminated,
 };
 use std::{
     io::{self, Read, Write},
     mem::size_of,
     ops::Index,
 };
+
+// TODO(Unavailable): Rename to symbols (functions/variables) from debug build.
 
 #[allow(dead_code)]
 enum PmanFileType {
@@ -43,7 +48,7 @@ impl PmanFileData {
     }
 
     pub fn to_zlib(&self) -> Option<Vec<u8>> {
-        (self.bytes[..2] == [b'Z', b'L']).then(|| {
+        (&self.bytes[..2] == b"ZL").then(|| {
             let size = u32::from_le_bytes([self.bytes[2], self.bytes[3], self.bytes[4], 0]);
             let mut decoder = ZlibDecoder::new(&self.bytes[5..]);
             let mut zlib = Vec::<u8>::with_capacity(size as usize);
@@ -57,31 +62,34 @@ impl PmanFileData {
     }
 }
 
-const HEADER_MAGIC_STRING: [u8; 4] = [b'P', b'M', b'A', b'N'];
-const COPYRIGHT_MAX_SIZE: usize = 56;
+const HEADER_SIZE: usize = 64;
+const HEADER_MAGIC_STRING: &[u8; 4] = b"PMAN";
+const COPYRIGHT_MAX_SIZE: usize = HEADER_SIZE - 9;
 
-fn parse_header(input: &[u8]) -> Result<(String, u32)> {
-    let (input, _) = tag(HEADER_MAGIC_STRING)(input)?;
-    let (input, file_count) = le_u32(input)?;
-    let (input, copyright) = take_till(|b| b == 0)(input)?;
-    // FIX(Unavailable): assert that copyright.len() is not bigger than 56.
-    let (input, _) = take(COPYRIGHT_MAX_SIZE - copyright.len())(input)?;
+fn read_header(input: &[u8]) -> Result<(String, u32)> {
+    const NULL: char = '\0';
 
-    Ok((
-        input,
-        (String::from_utf8_lossy(copyright).into(), file_count),
-    ))
+    let (input, header) = take(HEADER_SIZE)(input)?;
+    let (header, _) = tag(HEADER_MAGIC_STRING)(header)?;
+    let (header, file_count) = le_u32(header)?;
+    let (header, copyright) = terminated(take(COPYRIGHT_MAX_SIZE), char(NULL))(header)?;
+    _ = eof(header)?; // Not really needed, but having a guard doesn't hurt.
+
+    let copyright = String::from_utf8_lossy(copyright);
+    let copyright = copyright.trim_end_matches(NULL);
+
+    Ok((input, (copyright.into(), file_count)))
 }
 
-fn get_entry_table_size(file_count: u32) -> usize {
+fn entry_table_size(file_count: u32) -> usize {
     file_count as usize * size_of::<u32>() * 4
 }
 
-fn parse_entry_table(input: &[u8], file_count: u32) -> Result<&[u8]> {
-    take(get_entry_table_size(file_count))(input)
+fn read_entry_table(input: &[u8], file_count: u32) -> Result<&[u8]> {
+    take(entry_table_size(file_count))(input)
 }
 
-fn parse_files<'a>(
+fn read_files<'a>(
     input: &'a [u8],
     entry_table: &'a [u8],
     file_count: u32,
@@ -127,9 +135,9 @@ impl PmanFile {
     pub fn new(bytes: Vec<u8>) -> eyre::Result<PmanFile> {
         // needed to infer the err case of `?`.
         fn _new(bytes: &[u8]) -> Result<PmanFile> {
-            let (input, (copyright, file_count)) = parse_header(bytes)?;
-            let (input, entry_table) = parse_entry_table(input, file_count)?;
-            let (input, files) = parse_files(input, entry_table, file_count)?;
+            let (input, (copyright, file_count)) = read_header(bytes)?;
+            let (input, entry_table) = read_entry_table(input, file_count)?;
+            let (input, files) = read_files(input, entry_table, file_count)?;
             // FIX(Unavailable): assert input is empty.
 
             Ok((input, PmanFile { copyright, files }))
@@ -146,7 +154,8 @@ impl PmanFile {
     ///
     /// # Panics
     ///
-    /// If the new copyright string length is `>` than 56.
+    /// If the new copyright string length is `>` than 55. Do note that `length` != `# of chars`;
+    /// you can read [`String::len`] for more information.
     pub fn set_copyright<S>(&mut self, copyright: S)
     where
         S: Into<String>,
@@ -155,7 +164,7 @@ impl PmanFile {
 
         assert!(
             copyright.len() <= COPYRIGHT_MAX_SIZE,
-            "copyright notice should be less than 56 characters long."
+            "copyright notice should be less than {COPYRIGHT_MAX_SIZE} bytes long."
         );
 
         self.copyright = copyright;
@@ -170,10 +179,7 @@ impl PmanFile {
     }
 
     pub fn size_upto_file_data(&self) -> usize {
-        // The header should always be 64 bytes long.
-        const HEADER_SIZE: usize = 64;
-
-        HEADER_SIZE + get_entry_table_size(self.files.len() as u32)
+        HEADER_SIZE + entry_table_size(self.files.len() as u32)
     }
 
     /// Turns this `PmanFile` back to its bytes representation.
@@ -189,12 +195,13 @@ impl PmanFile {
         // state beforehand.
         let mut buf = Vec::with_capacity(size + files_size);
 
-        buf.write_all(&HEADER_MAGIC_STRING)?;
+        buf.write_all(HEADER_MAGIC_STRING)?;
         buf.write_all(&(self.files.len() as u32).to_le_bytes())?;
         buf.write_all(&self.copyright.as_bytes())?;
 
         let zero_bytes = [0; 1];
-        (0..COPYRIGHT_MAX_SIZE - self.copyright.len())
+        // + 1 to include a null character.
+        (0..COPYRIGHT_MAX_SIZE - self.copyright.len() + 1)
             .try_for_each(|_| buf.write_all(&zero_bytes))?;
 
         let zero_bytes = [0; 4];
@@ -246,8 +253,8 @@ mod tests {
     const ENTRY_TABLE_START: usize = 0x40;
 
     #[test]
-    fn header_test() -> eyre::Result<()> {
-        let (_, (copyright, file_count)) = parse_header(INPUT)?;
+    fn read_header_test() -> eyre::Result<()> {
+        let (_, (copyright, file_count)) = read_header(INPUT)?;
 
         assert_eq!(copyright, "Copyright (c) 2004 Torus Games Pty. Ltd.");
         assert_eq!(file_count, FILE_COUNT);
@@ -256,8 +263,8 @@ mod tests {
     }
 
     #[test]
-    fn entry_table_test() -> eyre::Result<()> {
-        let (_, entry_table) = parse_entry_table(&INPUT[ENTRY_TABLE_START..], FILE_COUNT)?;
+    fn read_entry_table_test() -> eyre::Result<()> {
+        let (_, entry_table) = read_entry_table(&INPUT[ENTRY_TABLE_START..], FILE_COUNT)?;
         let first_entry: [u32; 4] = {
             let slice = &entry_table[..size_of::<u32>() * 4];
 
@@ -270,14 +277,14 @@ mod tests {
     }
 
     #[test]
-    fn files_test() -> eyre::Result<()> {
+    fn read_files_test() -> eyre::Result<()> {
         let first_file = &INPUT[0xA20..];
         let entry_table = &INPUT[ENTRY_TABLE_START..];
 
-        let (_, files) = parse_files(first_file, entry_table, FILE_COUNT)?;
+        let (_, files) = read_files(first_file, entry_table, FILE_COUNT)?;
         let file = files[77].to_zlib().expect("zlib file data.");
 
-        assert_eq!(file[..4], [b'C', b'O', b'L', b'L']);
+        assert_eq!(&file[..4], b"COLL");
 
         Ok(())
     }
@@ -290,7 +297,7 @@ mod tests {
     }
 
     #[test]
-    fn into_bytes_test() -> eyre::Result<()> {
+    fn pman_into_bytes_test() -> eyre::Result<()> {
         let pman = PmanFile::new(INPUT.to_vec())?;
         let bytes = &pman.into_bytes()?;
 
